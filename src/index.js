7bdae8b648ab9b5e51a1ffe2581831e9d107fa34
Asmodeus <kirill6satur@gmail.com>
2026-04-22 18:01:33 +0300

    Recycling

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-'use strict'
+'use strict';
 
-function fatherOlder(ageFather, ageSon){
-    if(ageSon < 0 || ageFather - ageSon <= 15){
+function fatherOlder(ageFather, ageSon) {
+    if (ageFather < 0 || ageSon < 0 || (ageFather - ageSon) < 15) {
         return NaN;
     }
+    return 2 * ageSon - ageFather;
 }
 
-const ageFather = 59;
-const ageSon = 'ewgewgwegwre';
-const years = 2 * ageSon - ageFather;
+const ageFather = 61;
+const ageSon = 30;
+
+const years = fatherOlder(ageFather, ageSon);
 const yearsAbs = Math.abs(years);
 
 let yearsStr;
 
-if(yearsAbs >= 10 && yearsAbs <= 20 || yearsAbs > 21 && yearsAbs <= 30){
+if (yearsAbs % 100 >= 11 && yearsAbs % 100 <= 14) {
     yearsStr = 'років';
 } else {
-    switch (String(years).slice(-1)){
-        case '1':
+    switch (yearsAbs % 10) {
+        case 1:
             yearsStr = 'рік';
             break;
-        case '2':
-        case '3':
-        case '4':
+        case 2:
+        case 3:
+        case 4:
             yearsStr = 'роки';
             break;
         default:
@@ -30,12 +32,12 @@ if(yearsAbs >= 10 && yearsAbs <= 20 || yearsAbs > 21 && yearsAbs <= 30){
     }
 }
 
-switch(true){
-    case (Number.isNaN(years)): console.log(NaN);
-        break;
-    case (years > 0): console.log(`Батько буде вдвічі старший за сина через ${years} ${yearsStr}`);
-        break;
-    case (years < 0): console.log(`Батько був вдвічі старший за сина ${yearsAbs} ${yearsStr} тому`);
-        break;
-    default: console.log('Батько вже вдвічі старший за сина');
+if (Number.isNaN(years)) {
+    console.log(NaN);
+} else if (years > 0) {
+    console.log(`Батько буде вдвічі старший за сина через ${years} ${yearsStr}`);
+} else if (years < 0) {
+    console.log(`Батько був вдвічі старший за сина ${yearsAbs} ${yearsStr} тому`);
+} else {
+    console.log('Батько вже вдвічі старший за сина');
 }
